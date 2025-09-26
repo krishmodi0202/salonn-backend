e0d1fbcbbf964d15231d69547a45cb3aee48b090
krishmodi0202 <krishmodi33@gmail.com>
2025-09-26 14:02:10 +0530

    backend updated

--- routes/booking.js
+++ routes/booking.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Booking = require('../models/booking');
+const Booking = require('../models/Booking');
 
 // GET all bookings
 router.get('/', async (req, res) => {
